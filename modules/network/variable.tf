9a32f29f4e1c77d7b37fbace880da70eb975da57
BasavarajN12 <basunuchhi12@gmail.com>
2025-09-27 22:01:11 +0530

    Update variable.tf

--- modules/network/variable.tf
+++ modules/network/variable.tf
@@ -1,8 +1,38 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "vnet_name" {}
-variable "vnet_address_space" {}
-variable "subnet_name" {}
-variable "subnet_prefix" {}
-variable "nic_name" {}
-variable "nsg_name" {}
+variable "resource_group_name" {
+  description = "Enter resource group name"
+  type = string
+}
+
+variable "location" {
+  description = "Enter the location or region name for resource creation"
+  type = string
+}
+
+variable "vnet_name" {
+  description = "Enter name for vnet"
+}
+
+variable "vnet_address_space" {
+  description = "Enter the vnet_address_space for vnet"
+  type = string
+}
+
+variable "subnet_name" {
+  description = "Enter the subnet name for your vnet"
+  type = string
+}
+
+variable "subnet_prefix" {
+  description = "Enter subnet_prefix"
+  type = string
+}
+
+variable "nic_name" {
+  description = "Enter nic name"
+  type = string
+}
+
+variable "nsg_name" {
+  description = "Enter nsg name"
+  type = string
+}
