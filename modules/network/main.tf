aca97cab6a3ed108c3508a2e0eee7230a7628df5
BasavarajN12 <basunuchhi12@gmail.com>
2025-09-27 21:40:34 +0530

    Create main.tf

---
+++ modules/network/main.tf
@@ -0,0 +1,69 @@
+resource "azurerm_resource_group" "private" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.private.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.private.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_prefix]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.private.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.private.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+output "nic_id" {
+  value = azurerm_network_interface.nic.id
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.private.name
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.subnet.id
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
+}
